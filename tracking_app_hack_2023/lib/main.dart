c36e4d79cbb8dd0765f5ad4794f226e19cfffd3b
adrian-szwejk <aszwe2@illinois.edu>
2023-02-24 22:19:05 -0600

    added functionality to put circles on map when user changes lat/long location

--- tracking_app_hack_2023/lib/main.dart
+++ tracking_app_hack_2023/lib/main.dart
@@ -37,7 +37,7 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
   final Completer<GoogleMapController> _controller = Completer();
   static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
   static const LatLng destination = LatLng(37.33429383, -122.06600055);
-
+  Set<LatLng> loc = Set.from([]);
   LocationData? currentLocation;
 
   void getCurrentLocation() async {
@@ -62,10 +62,31 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
             ),
           ),
         );
+        debugPrint("new location: $newLoc");
+        if (!loc.contains(LatLng(newLoc.latitude!, newLoc.longitude!))) {
+          circles.add(
+            Circle(
+              circleId: CircleId(circles.length.toString()),
+              center: LatLng(
+                newLoc.latitude!,
+                newLoc.longitude!,
+              ),
+              radius: 100,
+              fillColor: Colors.blue.withOpacity(0.5),
+              strokeWidth: 0,
+            ),
+          );
+          loc.add(LatLng(newLoc.latitude!, newLoc.longitude!));
+        }
+
+        debugPrint(circles.length.toString());
+        debugPrint(circles.first.center.toString());
         setState(() {});
       },
     );
   }
+
+  Set<Circle> circles = Set.from([]);
 
   @override
   void initState() {
@@ -77,33 +98,36 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("test", style: const TextStyle(fontSize: 12)),
+        title: Text(widget.title, style: const TextStyle(fontSize: 12)),
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target:
-              LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
-          zoom: 13.5,
-        ),
-        markers: {
-          Marker(
-            markerId: const MarkerId("currentLocation"),
-            position:
-                LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
-          ),
-          const Marker(
-            markerId: MarkerId("source"),
-            position: sourceLocation,
-          ),
-          const Marker(
-            markerId: MarkerId("destination"),
-            position: destination,
-          ),
-        },
-        onMapCreated: (mapController) {
-          _controller.complete(mapController);
-        },
-      ),
+      body: currentLocation == null
+          ? const Center(child: Text("Loading"))
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                    currentLocation!.latitude!, currentLocation!.longitude!),
+                zoom: 13.5,
+              ),
+              markers: {
+                Marker(
+                  markerId: const MarkerId("currentLocation"),
+                  position: LatLng(
+                      currentLocation!.latitude!, currentLocation!.longitude!),
+                ),
+                const Marker(
+                  markerId: MarkerId("source"),
+                  position: sourceLocation,
+                ),
+                const Marker(
+                  markerId: MarkerId("destination"),
+                  position: destination,
+                ),
+              },
+              onMapCreated: (mapController) {
+                _controller.complete(mapController);
+              },
+              circles: circles,
+            ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
           children: [
@@ -114,7 +138,7 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
             const Spacer(),
             IconButton(
               icon: const Icon(Icons.search),
-              onPressed: () {},
+              onPressed: getCurrentLocation,
             ),
           ],
         ),
